b66c320e7e9dedb83afc9190baad27e221828c53
Ombretta Strafforello <ombretta.st@gmail.com>
2020-09-21 16:06:05 +0200

    added code to test models

---
+++ test_models.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 15:28:21 2020
+
+@author: ombretta
+"""
+
+import torchvision
+import torch 
+
+import bagnets.pytorchnet
+
+import tqdm
+
+def validation(valid_loader, model, device, criterion):
+                
+    model.eval()
+    running_valid_loss, total_valid, correct_valid = 0, 0, 0
+    
+    with torch.no_grad():
+        for batch_X, batch_y in tqdm(valid_loader):
+            
+            batch_X = batch_X.to(device)
+            batch_y = batch_y.to(device)
+        
+            outputs, out_logits = model(batch_X) 
+            labels = batch_y.long().to(outputs.device)
+            loss = criterion(out_logits, labels) 
+            _, predictions = torch.max(outputs.detach(), 1)
+           
+            running_valid_loss += loss.item() 
+            total_valid += batch_y.shape[0]
+            correct_valid += predictions.eq(labels).sum().item()  
+            
+    return running_valid_loss/len(valid_loader), correct_valid/total_valid 
+
+
+
+device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+imagenet_val_dir = "/;/tudelft.net/staff-bulk/ewi/insy/VisionLab/osk/imagenet_training/imagenet_500/val/"
+imagenet_data = torchvision.datasets.ImageNet(imagenet_val_dir, split="val")
+imagenet_data_loader = torch.utils.data.DataLoader(imagenet_data,
+                                          batch_size=1,
+                                          shuffle=False)
+## BAGNET TESTING ON IMAGENET ##
+# available are bagnet9, bagnet17 and bagnet33
+
+models = [bagnets.pytorchnet.bagnet9, 
+          bagnets.pytorchnet.bagnet17, 
+          bagnets.pytorchnet.bagnet33]
+
+criterion = torch.nn.CrossEntropyLoss()
+
+for model in models:
+    print(model)
+    pytorch_model = model(pretrained=True) 
+    test_loss, test_acc = validation(imagenet_data_loader, model, device, criterion)
+        
+    print("Test loss", test_loss)
+    print("Test accuracy", test_acc)
